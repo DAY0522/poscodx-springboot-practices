2320a272e3327ed2fc965aecbe59e6b6a1384129
DAY0522 <ekdud_____@naver.com>
2025-01-20 16:56:33 +0900

    springboot #1

--- springboot-example/src/main/java/ex01/ExApplication.java
+++ springboot-example/src/main/java/ex01/ExApplication.java
@@ -2,10 +2,35 @@ package ex01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class ExApplication { // Bootstrapping Class
+
+    @Bean
+    public Map<?, ?> myMap() {
+        return Map.of("greetings", "Hello World");
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(ExApplication.class, args);
+        /*
+        ConfigurableApplicationContext ac = null;
+        try {
+            ac = SpringApplication.run(ExApplication.class, args);
+            System.out.println("ExApplication runs... " + ac.getBean("myMap", Map.class).get("greetings"));
+        } catch (Exception ignored) {
+        } finally {
+            ac.close();
+        }
+        */
+
+        // 가장 정석인 springboot 코드
+        try (ConfigurableApplicationContext ac = SpringApplication.run(ExApplication.class, args)) {
+            System.out.println("ExApplication runs... " + ac.getBean("myMap", Map.class).get("greetings"));
+        }
     }
 }
